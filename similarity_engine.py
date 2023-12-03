8cff5d03b7d338e5d9cd36c6b0efa5efa15f569b
kamda-cyrial <kamdacyrial@gmail.com>
2023-12-03 11:38:42 -0500

    appropriately naming gpt3.5turbo model

--- similarity_engine.py
+++ similarity_engine.py
@@ -63,7 +63,7 @@ def get_similarity_score(statements_1, statements_2):
                 "content": create_json_matrix_prompt(statements_1, statements_2),
             }
         ],
-        model="GPT-3.5 Turbo",
+        model="gpt-3.5-turbo",
         timeout=60,
     )
     return chat_completion.choices[0].message.content
